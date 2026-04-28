5024c6fc3fd419ffb87de51a11ea19bdf56411b2
dewstream-kr <dewstream.kr@gmail.com>
2026-04-28 17:57:18 +0900

    Create rwro_route.h

---
+++ include/rwro_route.h
@@ -0,0 +1,5 @@
+/* Compatibility wrapper for the RW/RO routing core. SPDX-License-Identifier: ISC */
+#ifndef PGBOUNCER_RWRO_ROUTE_WRAPPER_H
+#define PGBOUNCER_RWRO_ROUTE_WRAPPER_H
+#include "routing.h"
+#endif
